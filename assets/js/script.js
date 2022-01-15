3fda6f12659a83e8f30b9365435eee8e016a5020
claudia.davis <claudiacdavis@gmail.com>
2022-01-15 16:08:59 -0500

    fixed high score page

--- assets/js/script.js
+++ assets/js/script.js
@@ -154,7 +154,7 @@ function saveHighscore() {
     window.localStorage.setItem("highscores", JSON.stringify(highscores));
 
     // redirect to next page
-    window.location.href = "score.html";
+    window.location.href = "highscores.html";
   }
 }
 
