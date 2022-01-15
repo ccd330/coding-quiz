042a28abb0a1452396f6384697a5ced3d1fd0632
claudia.davis <claudiacdavis@gmail.com>
2022-01-14 21:16:13 -0500

    initial html and questions

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+//array with questions
+let questions = [
+  {
+    numb: 1,
+    question: "Commonly used data types DO NOT include",
+    answer: "Hyper Text Markup Language",
+    options: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+  },
+  {
+    numb: 2,
+    question: "The condition in an if/else statement is enclosed with: ",
+    answer: "Cascading Style Sheet",
+    options: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parentheses",
+      "4. square brackets",
+    ],
+  },
+  {
+    numb: 3,
+    question: "Arrays in JavaScript can be used to store:",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above",
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      "String values must be enclosed within BLANK when being assigned to variables.",
+    answer: "Structured Query Language",
+    options: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"],
+  },
+  {
+    numb: 5,
+    question:
+      "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: "eXtensible Markup Language",
+    options: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops",
+      "4. console.log",
+    ],
+  },
+];
